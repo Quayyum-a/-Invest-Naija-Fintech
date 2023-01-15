ae74c74ba4b1cb72caf8505ce889ad7b4b277d8c
Quayyum <quayyumariyo@gmail.com>
2023-01-15 18:42:00 +0100

    Add new pages #98

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -310,3 +310,4 @@ export const getCurrentUser: RequestHandler = (req, res) => {
 // Past year commit 48 - 1752189510
 // Past year commit 56 - 1752189511
 // Past year commit 80 - 1752189513
+// Past year commit 98 - 1752189516
