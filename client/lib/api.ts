389318c9294645d253595a31da237a8e7e520454
Quayyum <quayyumariyo@gmail.com>
2023-01-16 09:22:00 +0100

    Fix bugs and issues #102

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -720,3 +720,4 @@ export const apiService = new ApiService();
 // Past year commit 77 - 1752189513
 // Past year commit 87 - 1752189514
 // Past year commit 91 - 1752189515
+// Past year commit 102 - 1752189516
