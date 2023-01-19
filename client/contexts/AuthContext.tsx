dd9b29db1c8971439421cf5d11c19ac637884d10
Quayyum <quayyumariyo@gmail.com>
2023-01-19 17:03:00 +0100

    Add new services #121

--- client/contexts/AuthContext.tsx
+++ client/contexts/AuthContext.tsx
@@ -329,3 +329,4 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 // Past year commit 65 - 1752189512
 // Past year commit 82 - 1752189513
 // Past year commit 111 - 1752189517
+// Past year commit 121 - 1752189518
