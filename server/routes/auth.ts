1b919e5fc34909d1862ca2b34e0a745392f25458
Quayyum <quayyumariyo@gmail.com>
2023-01-28 13:29:00 +0100

    Update documentation #189

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -320,3 +320,4 @@ export const getCurrentUser: RequestHandler = (req, res) => {
 // Past year commit 157 - 1752189523
 // Past year commit 162 - 1752189524
 // Past year commit 179 - 1752189527
+// Past year commit 189 - 1752189528
