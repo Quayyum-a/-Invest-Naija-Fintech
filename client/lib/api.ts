64fab7b825636653b0ce4ff4df69a5a1410b4e5e
Quayyum <quayyumariyo@gmail.com>
2023-02-01 10:48:00 +0100

    Fix mobile responsiveness #205

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -729,3 +729,4 @@ export const apiService = new ApiService();
 // Past year commit 186 - 1752189527
 // Past year commit 187 - 1752189528
 // Past year commit 200 - 1752189529
+// Past year commit 205 - 1752189530
