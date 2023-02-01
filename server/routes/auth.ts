c140a80e430f674df7552dfcf0a59ac1d1243f88
Quayyum <quayyumariyo@gmail.com>
2023-02-01 09:50:00 +0100

    Add API endpoints #204

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -323,3 +323,4 @@ export const getCurrentUser: RequestHandler = (req, res) => {
 // Past year commit 189 - 1752189528
 // Past year commit 198 - 1752189529
 // Past year commit 203 - 1752189530
+// Past year commit 204 - 1752189530
