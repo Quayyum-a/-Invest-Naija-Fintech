cefa98e2d486e4f4a76bc2e5a6555f75a9402b71
Quayyum <quayyumariyo@gmail.com>
2023-02-13 17:04:00 +0100

    Add validation logic #294

--- server/index.ts
+++ server/index.ts
@@ -535,3 +535,4 @@ export function createServer() {
 // Past year commit 244 - 1752189535
 // Past year commit 274 - 1752189538
 // Past year commit 277 - 1752189538
+// Past year commit 294 - 1752189541
