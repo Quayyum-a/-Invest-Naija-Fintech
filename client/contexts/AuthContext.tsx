8203f56d1ffd226431922d93ee922e9e908a0a5f
Quayyum <quayyumariyo@gmail.com>
2023-02-20 16:25:00 +0100

    Improve UI/UX #337

--- client/contexts/AuthContext.tsx
+++ client/contexts/AuthContext.tsx
@@ -348,3 +348,4 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 // Past year commit 298 - 1752189541
 // Past year commit 321 - 1752189544
 // Past year commit 336 - 1752189545
+// Past year commit 337 - 1752189545
