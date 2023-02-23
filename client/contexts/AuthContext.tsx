32c2fa7c14a9c19b75af61b1c7e3f1ea4a2af943
Quayyum <quayyumariyo@gmail.com>
2023-02-24 00:00:00 +0100

    Create resolution tracking #171

--- client/contexts/AuthContext.tsx
+++ client/contexts/AuthContext.tsx
@@ -292,3 +292,4 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 // 2023 commit 149 - 1752189237
 // 2023 commit 152 - 1752189238
 // 2023 commit 157 - 1752189239
+// 2023 commit 171 - 1752189243
