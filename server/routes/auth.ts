2efec1c7f107827e1c67f09277622d20b728da80
Quayyum <quayyumariyo@gmail.com>
2023-05-01 00:00:00 +0100

    Add proof of work #301

--- server/routes/auth.ts
+++ server/routes/auth.ts
@@ -288,3 +288,4 @@ export const getCurrentUser: RequestHandler = (req, res) => {
 // 2023 commit 277 - 1752189259
 // 2023 commit 294 - 1752189263
 // 2023 commit 298 - 1752189264
+// 2023 commit 301 - 1752189264
