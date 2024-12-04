f7b00007b009bce34168f243e5e9de6adce6862b
Quayyum <quayyumariyo@gmail.com>
2024-12-04 13:05:00 +0100

    Fix navigation issue #65

--- client/contexts/AuthContext.tsx
+++ client/contexts/AuthContext.tsx
@@ -319,3 +319,4 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 // December commit 24 - 1752189483
 // December commit 40 - 1752189487
 // December commit 54 - 1752189489
+// December commit 65 - 1752189491
