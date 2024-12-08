5239134b5f14d91a539f70932e4a41a1a14d7831
Quayyum <quayyumariyo@gmail.com>
2024-12-08 10:38:00 +0100

    Update styling #38

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -696,3 +696,4 @@ export const apiService = new ApiService();
 // December commit 30 - 1752189485
 // December commit 35 - 1752189486
 // December commit 37 - 1752189486
+// December commit 38 - 1752189486
