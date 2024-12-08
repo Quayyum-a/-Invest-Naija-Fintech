4882e15c230cdd301ae22d702d52bfb84f0552d3
Quayyum <quayyumariyo@gmail.com>
2024-12-08 12:40:00 +0100

    Fix responsive design #100

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -704,3 +704,4 @@ export const apiService = new ApiService();
 // December commit 70 - 1752189492
 // December commit 74 - 1752189493
 // December commit 92 - 1752189495
+// December commit 100 - 1752189496
