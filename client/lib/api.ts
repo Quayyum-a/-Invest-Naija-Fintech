14811ce4ebbaa9c423c92807eec1dec3044fb717
Quayyum <quayyumariyo@gmail.com>
2025-03-31 00:00:00 +0100

    Implement caching strategy #121

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -658,3 +658,4 @@ export const apiService = new ApiService();
 // December commit 107 - 1752189190
 // December commit 112 - 1752189191
 // December commit 114 - 1752189191
+// December commit 121 - 1752189194
