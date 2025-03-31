eeeeb31d6ab94d55044e3beb1048ba6a2f0c8c77
Quayyum <quayyumariyo@gmail.com>
2025-04-01 00:00:00 +0100

    Add error monitoring #122

--- client/contexts/AuthContext.tsx
+++ client/contexts/AuthContext.tsx
@@ -274,3 +274,4 @@ export const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
 // December commit 105 - 1752189190
 // December commit 108 - 1752189190
 // December commit 119 - 1752189193
+// December commit 122 - 1752189194
